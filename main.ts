67502efad99de904dc4c93628993a1875b70b09a
Mariasharifuddin <161883333+Mariasharifuddin@users.noreply.github.com>
2024-05-27 15:57:58 +0500

    push my count down to github

---
+++ main.ts
@@ -0,0 +1,46 @@
+#! /usr/bin/env node
+// project 9 in this project, you will build a countdown timer using the date module.
+import{ differenceInSeconds } from 'date-fns';
+import inquirer from 'inquirer'
+
+const res =await inquirer.prompt({
+    type: "number",
+    name: "userInput",
+    message: "Enter amount of Second",
+    validate:(input) => {
+        if (isNaN(input)){
+            return"please enter valid number"
+        }else if (input>60){
+            return "Second must within 60"
+
+        }else{
+            return true;
+        }
+
+    
+    }
+});
+
+let input =res.userInput
+
+function startTime(val:number) {
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val)
+    const intervalTime = new Date(intTime);
+    setInterval(()=>{
+        const currentTime = new Date()
+        const timeDiff = differenceInSeconds(intervalTime,currentTime)
+        if(timeDiff <=0){
+            console.log('Time has expired')
+            process.exit();
+        }
+        const min = Math.floor((timeDiff%(3600*24))/3600)
+        const sec = Math.floor(timeDiff% 60);
+        console.log(`${min.toString().padStart(2,"0")}:${sec.toString().padStart(2,"0")}`)
+    }, 1000);
+}
+
+
+startTime(input);
+     
+
+
